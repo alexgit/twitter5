a572a749ff4597c715d5e311c479e369134c780d
alexgit <alex.tashev@gmail.com>
2012-12-15 13:14:56 +0000

    The feed now keeps only X days worth of tweets.

--- js/twitter-feed.js
+++ js/twitter-feed.js
@@ -4,7 +4,22 @@
 	if(!localStorage)
 		throw "localStorage not supported"
 
-	function TwitterFeed(key) {
+	function TwitterFeed(key, days) {
+		days = days || 2;
+
+		function trim(tweets) {
+			var now = new Date();
+			var cutoffDate = new Date();
+			cutoffDate.setDate(now.getDate() - 2);
+
+			for (var i = tweets.length - 1; i >= 0; i--) {
+				if(tweets[i].date > cutoffDate) {					
+					return tweets.slice(0, i + 1);
+				}
+			}
+
+			return tweets;
+		}
 		
 		if(!localStorage[key]) {
 			localStorage[key] = JSON.stringify([]);
@@ -21,7 +36,8 @@
 				currentTweets.unshift(tweets[i]);
 			}
 
-			localStorage[key] = JSON.stringify(currentTweets);
+			var trimmed = trim(currentTweets);
+			localStorage[key] = JSON.stringify(trimmed);
 		}
 
 		this.addTweet = function(tweet) {
